54be4ed9238b5c08db096ac214024dc4145b117a
SecurityEasy <seasycdmx@gmail.com>
2025-05-28 14:00:46 -0600

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const premios = [
   "1 R83 + 5 Sims Telcel"
 ];
 
-const colors = ["#c21d2e", "#f6b942", "#e3d1b1", "#851c1c", "#f27a3f"];
+const colors = ["#b30000", "#f4cccc", "#f5f5dc", "#b30000", "#f4cccc"];
 
 const canvas = document.getElementById("wheel");
 const ctx = canvas.getContext("2d");
